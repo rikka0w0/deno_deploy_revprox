e33ecc7b0ffe9a34fad706e51254ca2139a85ef5
rikka <929513338@qq.com>
2023-11-22 00:44:12 +1100

    Add more WebSocket handling

--- server.ts
+++ server.ts
@@ -1,20 +1,57 @@
-Deno.serve((req) => {
+const portalChannel = new BroadcastChannel("portal");
 
-    if (req.headers.get("upgrade") != "websocket") {
-      return new Response(null, { status: 501 });
-    }
-  
-    const { socket, response } = Deno.upgradeWebSocket(req);
-  
-    socket.addEventListener("open", () => {
-      console.log("a client connected!");
-    });
-  
-    socket.addEventListener("message", (event) => {
-      if (event.data === "ping") {
-        socket.send("pong");
-      }
-    });
-  
-    return response;
-  });
+enum PortalMessageType {
+	WebSocketCreate = 1,
+	WebSocketPacket = 2,
+	WebSocketClose = 3,
+}
+
+interface PortalMessage {
+	identifier: string;
+	type: PortalMessageType;
+}
+
+interface PortalRequest extends PortalMessage {
+}
+
+interface PortalResponse extends PortalMessage {
+
+}
+
+function handleWsOut(websocket: WebSocket):void {
+	websocket.addEventListener("open", () => {
+		console.log("a client connected!");
+	});
+	
+	websocket.addEventListener("message", (event) => {
+		if (event.data === "ping") {
+			websocket.send("pong");
+		}
+	});
+}
+
+function handleWsIn(websocket: WebSocket):void {
+
+}
+
+function handleWsUpgrade(req: Request, wsHandler: (websocket: WebSocket) => void): Response {
+	if (req.headers.get("upgrade") != "websocket") {
+		return new Response(null, { status: 501 });
+	}
+
+	const upgradeResult = Deno.upgradeWebSocket(req);
+	wsHandler(upgradeResult.socket);
+	return upgradeResult.response;
+}
+
+Deno.serve((req) => {
+	const reqURL = new URL(req.url);
+	switch (reqURL.pathname) {
+		case '/ws_out':
+			return handleWsUpgrade(req, handleWsOut);
+		case '/ws_in':
+			return handleWsUpgrade(req, handleWsIn);
+		default:
+			return new Response('Http Server!', { status: 200 });
+	}
+});
